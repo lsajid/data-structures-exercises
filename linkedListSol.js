c234eb336904e78fba8a61a79dfffd8c3fd7c081
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-04-06 17:52:46 -0400

    some linkedlist prob

---
+++ linkedListSol.js
@@ -0,0 +1,41 @@
+const { nums, words } = require("./data/data.js");
+import util from "util";
+
+//create a Class node.
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+//here is a class Linked list
+//insert at beginning 
+//insert at location
+//insert at end
+//get the size
+//delete at index
+//remove element 
+//get the index of element
+//is empty
+//print the list
+class LinkedList {
+    constructor(head = null){
+        this.head = head;
+    }
+    insertBeginning(){
+
+    }
+    insertEnd(){
+
+    }
+    insertAt(){
+
+    }
+    size(){
+
+    }
+    delete(){
+
+    }
+}
